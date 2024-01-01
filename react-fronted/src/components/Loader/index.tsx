2ac6d494fe819cfe2fdb5ca378170008aa0d47c6
apurv-dolphin <apurv.dolphinwebsolution@gmail.com>
2024-01-01 12:10:35 +0530

    Added:loader while sending mail.

---
+++ react-fronted/src/components/Loader/index.tsx
@@ -0,0 +1,17 @@
+import "./loader.css";
+
+export default function Loader() {
+  return (
+    <section className="loader-container">
+      <div className="loading loading01">
+        <span>S</span>
+        <span>e</span>
+        <span>n</span>
+        <span>d</span>
+        <span>i</span>
+        <span>n</span>
+        <span>g</span>
+      </div>
+    </section>
+  );
+}
